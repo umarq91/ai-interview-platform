c1536326cabfc285a797420dd2275f2747e0032b
Muhammad Umar Qureshi <m.umarqureshi091@gmail.com>
2025-05-07 20:41:58 +0500

    Cards UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
-const HomePage = () => {
+import { Button } from "@/components/ui/button";
+import InterviewCard from "@/components/InterviewCard";
+import { dummyInterviews } from "@/constants";
+// import InterviewCard from "@/components/InterviewCard";
+
+// import { getCurrentUser } from "@/lib/actions/auth.action";
+// import {
+//   getInterviewsByUserId,
+//   getLatestInterviews,
+// } from "@/lib/actions/general.action";
+
+async function Home() {
+  // const user = await getCurrentUser();
+
+  // const [userInterviews, allInterview] = await Promise.all([
+  //   getInterviewsByUserId(user?.id!),
+  //   getLatestInterviews({ userId: user?.id! }),
+  // ]);
+
+  // const hasPastInterviews = userInterviews?.length! > 0;
+  // const hasUpcomingInterviews = allInterview?.length! > 0;
+
   return (
-    <div>HomePage</div>
-  )
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              key={interview.id}
+              userId={"1234213"}
+              interviewId={interview.id}
+              role={interview.role}
+              type={interview.type}
+              techstack={interview.techstack}
+              createdAt={interview.createdAt}
+            />
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take Interviews</h2>
+
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard
+              key={interview.id}
+              userId={"1234213"}
+              interviewId={interview.id}
+              role={interview.role}
+              type={interview.type}
+              techstack={interview.techstack}
+              createdAt={interview.createdAt}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default HomePage
+export default Home;
